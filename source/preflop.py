45a9ed23cd3d40c34931169afcdbbca07cc2be21
marcocasarta <marcocasarta@gmail.com>
2024-01-07 08:50:57 -0600

    Edited hand validation

--- source/preflop.py
+++ source/preflop.py
@@ -1,13 +1,45 @@
 def check_poker_move(Position, Hand, Action):
     # Check Position
-    if Position != 'UTG':
+    if Position != 'utg':
         return 'Error: Only UTG position is acceptable.'
 
     # Check Hand
-    # You can add more complex validation logic for poker hands if needed
     # For simplicity, we are assuming any two cards input is valid
+
+    #Valid cards
+    cardArray = ['a','k','q','j','2','3','4','5','6','7','8','9','t']
+    
+    valid = 0
+
+    #Checks first and third position for valid card values
+
+    for i in cardArray:
+        if Hand[0] == i:
+            valid += 1
+        if Hand[2] == i:
+            valid += 1
+
+    if valid != 2:
+        return 'Error: Invalid Poker Hand'      
+   
+   #Valid suits
+    
+    suitArray = ['s','d','h','c']
+
+    #Checks second and fourth position for valid suit values
+
+    for i in suitArray:
+        if Hand[1] == i:
+            valid += 1
+        if Hand[3] == i:
+            valid += 1
+
+    if valid != 4:
+        return 'Error: Invalid Poker Hand'  
+    
     if len(Hand) != 4:
-        return 'Error: Invalid poker hand.'
+        return 'Error: Invalid Poker Hand' 
+
 
     # Check Action
     if Action.lower() not in ['raise', 'fold']:
