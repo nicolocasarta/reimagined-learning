e7f4c00b41e01e9d5f576b1bc4d3ef502e23285b
anthonycasarta <anthonycasarta@gmail.com>
2024-03-23 17:54:03 -0500

    Add test for btn action

--- tests/action_check_test.py
+++ tests/action_check_test.py
@@ -11,3 +11,7 @@ def test_preflop_utg_action(input, expected_result):
 @pytest.mark.parametrize("input, expected_result", get_data("preflop_utg1_action_test.csv"))
 def test_preflop_utg1_action(input, expected_result):
     assert action_check.preflop_utg1_action(input) == expected_result 
+
+@pytest.mark.parametrize("input, expected_result", get_data("preflop_btn_action_test.csv"))
+def test_preflop_btn_action(input, expected_result):
+    assert action_check.preflop_btn_action(input) == expected_result
