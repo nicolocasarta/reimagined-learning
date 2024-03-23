000db1eb84418ed65953544592e37821dd4ac0b6
anthonycasarta <anthonycasarta@gmail.com>
2024-03-23 15:24:34 -0500

    Add test for LJ

--- tests/action_check_test.py
+++ tests/action_check_test.py
@@ -11,3 +11,6 @@ def test_preflop_utg_action(input, expected_result):
 @pytest.mark.parametrize("input, expected_result", get_data("preflop_utg1_action_test.csv"))
 def test_preflop_utg1_action(input, expected_result):
     assert action_check.preflop_utg1_action(input) == expected_result 
+
+def test_preflop_lj_action():
+    assert action_check.preflop_lj_action("aa") == "raise"
