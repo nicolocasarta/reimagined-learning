423a9e969288dfe600c2fde7d4f33f618d5adb3a
anthonycasarta <anthonycasarta@gmail.com>
2024-03-26 14:33:34 -0500

    Refactor action check and tests

--- source/action_check.py
+++ source/action_check.py
@@ -3,59 +3,22 @@ from source.utils.util import jason_to_dict
 # dictionary that holds all preflop proper actions for a given hand
 PREFLOP_ACTION = jason_to_dict("preflop_action.json")
 
+# get the correct action from a hand in a position
+def get_correct_action(position, hand):
+    # locate the correct action from a hand at the position
+    correct_action = PREFLOP_ACTION[position][hand]
+    return correct_action
+
+
 #check if user action matches the correct response
-def action_check(position, hand, action):
+def action_check(correct_action, user_action):
 
-    proper_action = PREFLOP_ACTION[position][hand]
-
-
-    if (action == proper_action):
-        print("Your decision to " + action.upper() + " was CORRECT!")
-        return True
+    if (user_action == correct_action):
+        print("Your decision to " + user_action.upper() + " was CORRECT!")
     else:
-        print("Your decision to " + action.upper() + " was INCORRECT!")
-        return False
+        print("Your decision to " + user_action.upper() + " was INCORRECT!")
 
 
-
-def preflop_utg_action(hand):
-    # get dictionary at key, "utg_action"
-    utg = PREFLOP_ACTION["utg"]
-    # get the action from the key associated with the hand
-    action = utg[hand]
-    
-    return action
-
-
-
-def preflop_utg1_action(hand):
-    # get dictionary at key, "utg1_action"
-    utg1 = PREFLOP_ACTION["utg1"]
-    # get the action from the key associated with the hand
-    action = utg1[hand]
-    
-    return action
-   
-
-
-
-def preflop_btn_action(hand):
-    # get dictionary at key, "btn_action"
-    btn = PREFLOP_ACTION["btn"]
-    # get the action from the key associated with the hand
-    action = btn[hand]
-    
-    return action
-
-
-
-def preflop_lj_action(hand):
-    # get dictionary at key, "lj_action"
-    lj = PREFLOP_ACTION["lj"]
-    # get the action from the key associated with the hand
-    action = lj[hand]
-    
-    return action
 
 
 
--- tests/action_check_test.py
+++ tests/action_check_test.py
@@ -4,18 +4,18 @@ from tests.utils.util import get_data
 
 
 
-@pytest.mark.parametrize("hand, action", get_data("preflop_utg_action_test.csv"))
-def test_preflop_utg_action(hand, action):
-    assert action_check.action_check("utg", hand,  action) == True
+@pytest.mark.parametrize("hand, expected_action", get_data("preflop_utg_action_test.csv"))
+def test_preflop_utg_action(hand, expected_action):
+    assert action_check.get_correct_action("utg", hand) == expected_action
 
-@pytest.mark.parametrize("input, expected_result", get_data("preflop_utg1_action_test.csv"))
-def test_preflop_utg1_action(input, expected_result):
-    assert action_check.preflop_utg1_action(input) == expected_result 
+@pytest.mark.parametrize("hand, expected_action", get_data("preflop_utg1_action_test.csv"))
+def test_preflop_utg1_action(hand, expected_action):
+    assert action_check.get_correct_action("utg1", hand) == expected_action
 
-@pytest.mark.parametrize("input, expected_result", get_data("preflop_btn_action_test.csv"))
-def test_preflop_btn_action(input, expected_result):
-    assert action_check.preflop_btn_action(input) == expected_result
+@pytest.mark.parametrize("hand, expected_action", get_data("preflop_btn_action_test.csv"))
+def test_preflop_btn_action(hand, expected_action):
+    assert action_check.get_correct_action("btn", hand) == expected_action
     
-@pytest.mark.parametrize("input, expected_result", get_data("preflop_lj_action_test.csv"))
-def test_preflop_lj_action(input, expected_result):
-    assert action_check.preflop_lj_action(input) == expected_result
+@pytest.mark.parametrize("hand, expected_action", get_data("preflop_lj_action_test.csv"))
+def test_preflop_lj_action(hand, expected_action):
+    assert action_check.get_correct_action("lj", hand) == expected_action
