9fcdaf1f492a304b690040cefc5936707287883b
anthonycasarta <anthonycasarta@gmail.com>
2024-03-26 11:56:39 -0500

    Refactor action_check function using json file

--- source/action_check.py
+++ source/action_check.py
@@ -6,28 +6,21 @@ PREFLOP_ACTION = jason_to_dict("preflop_action.json")
 #check if user action matches the correct response
 def action_check(position, hand, action):
 
-    if position == "utg":
-        proper_action = preflop_utg_action(hand)
-    if position == "utg+1":
-        proper_action = preflop_utg1_action(hand)
-
-    if position == "btn":
-        proper_action = preflop_btn_action(hand)
-
-    if position == "lj":
-        proper_action = preflop_lj_action(hand)
+    proper_action = PREFLOP_ACTION[position][hand]
 
 
     if (action == proper_action):
         print("Your decision to " + action.upper() + " was CORRECT!")
+        return True
     else:
         print("Your decision to " + action.upper() + " was INCORRECT!")
+        return False
 
 
 
 def preflop_utg_action(hand):
     # get dictionary at key, "utg_action"
-    utg = PREFLOP_ACTION["utg_action"]
+    utg = PREFLOP_ACTION["utg"]
     # get the action from the key associated with the hand
     action = utg[hand]
     
@@ -37,7 +30,7 @@ def preflop_utg_action(hand):
 
 def preflop_utg1_action(hand):
     # get dictionary at key, "utg1_action"
-    utg1 = PREFLOP_ACTION["utg1_action"]
+    utg1 = PREFLOP_ACTION["utg1"]
     # get the action from the key associated with the hand
     action = utg1[hand]
     
@@ -48,7 +41,7 @@ def preflop_utg1_action(hand):
 
 def preflop_btn_action(hand):
     # get dictionary at key, "btn_action"
-    btn = PREFLOP_ACTION["btn_action"]
+    btn = PREFLOP_ACTION["btn"]
     # get the action from the key associated with the hand
     action = btn[hand]
     
@@ -58,7 +51,7 @@ def preflop_btn_action(hand):
 
 def preflop_lj_action(hand):
     # get dictionary at key, "lj_action"
-    lj = PREFLOP_ACTION["lj_action"]
+    lj = PREFLOP_ACTION["lj"]
     # get the action from the key associated with the hand
     action = lj[hand]
     
--- tests/action_check_test.py
+++ tests/action_check_test.py
@@ -4,9 +4,9 @@ from tests.utils.util import get_data
 
 
 
-@pytest.mark.parametrize("input, expected_result", get_data("preflop_utg_action_test.csv"))
-def test_preflop_utg_action(input, expected_result):
-    assert action_check.preflop_utg_action(input) == expected_result
+@pytest.mark.parametrize("hand, action", get_data("preflop_utg_action_test.csv"))
+def test_preflop_utg_action(hand, action):
+    assert action_check.action_check("utg", hand,  action) == True
 
 @pytest.mark.parametrize("input, expected_result", get_data("preflop_utg1_action_test.csv"))
 def test_preflop_utg1_action(input, expected_result):
